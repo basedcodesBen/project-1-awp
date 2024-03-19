ab2e2c41faaa8790d5160f28f3debb4f47a1b216
Benaya Andrias <113487774+basedcodesBen@users.noreply.github.com>
2024-03-19 14:45:25 +0700

    added models, middleware, migrations

---
+++ database/migrations/2024_03_11_071454_create_polling_detail_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('polling_detail', function (Blueprint $table) {
+            $table->string('id_polling');
+            $table->foreign('id_polling')->references('id_polling')->on('polling');
+            $table->string('kode_matkul');
+            $table->foreign('kode_matkul')->references('kode_matkul')->on('mata_kuliah');
+            $table->string('nrp');
+            $table->foreign('nrp')->references('nrp')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('polling_detail');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users') -> insert([
+            'nrp' => '2272003',
+            'name' => 'Ravel Setiady',
+            'email' => '2272003@maranatha.ac.id',
+            'password' => bcrypt('stdy'),
+            'gender' => 'Men',
+            'birthdate' => '24/11/04',
+            'id_prodi' =>  '72',
+            'id_role' => '3'
+        ]);
+    }
+}
