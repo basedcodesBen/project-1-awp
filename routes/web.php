ff69b8cf5438f30a14445eeb35da5ba1df2cafbd
Benaya Andrias <113487774+basedcodesBen@users.noreply.github.com>
2024-03-19 14:16:47 +0700

    first main push

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/profile', 'ProfileController@index')->name('profile');
-Route::put('/profile', 'ProfileController@update')->name('profile.update');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/blank', function () {
-    return view('blank');
-})->name('blank');
-
-Route::middleware('auth')->group(function() {
-    Route::resource('basic', BasicController::class);
 });
